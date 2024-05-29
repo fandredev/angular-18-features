821b607680332cfa9c07f3527d4fe1d4149b984f
felipe <profissionalf.andre@gmail.com>
2024-05-28 22:27:55 -0300

    feat: add skeleton informations sections and create test from component

--- src/app/informations/informations.component.spec.ts
+++ src/app/informations/informations.component.spec.ts
@@ -5,10 +5,13 @@ import { InformationsComponent } from './informations.component';
 describe('InformationsComponent', () => {
   let component: InformationsComponent;
   let fixture: ComponentFixture<InformationsComponent>;
+  let nativeElement: HTMLElement;
 
   function createComponent() {
     fixture = TestBed.createComponent(InformationsComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
+
     fixture.detectChanges();
   }
 
@@ -22,5 +25,84 @@ describe('InformationsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a three sections to informations', () => {
+    const expectedSectionsQuantity = 3;
+    const sections = nativeElement.querySelectorAll(
+      'section'
+    ) as NodeListOf<HTMLElement>;
+
+    expect(sections.length).toBe(expectedSectionsQuantity);
+  });
+
+  describe('tests from section  about curriculum', () => {
+    const containerClass = '.container-informations-curriculum';
+
+    it('should have a title with contain text "currículo alinhado a padrões de aprendizagem"', () => {
+      const title = nativeElement.querySelector(
+        `${containerClass} h3`
+      ) as HTMLElement;
+
+      expect(title.textContent).toBe(
+        'currículo alinhado a padrões de aprendizagem'
+      );
+    });
+
+    it('should have a subtitle with contain text "O Duolingo for Schools utiliza..."', () => {
+      const subtitle = nativeElement.querySelector(
+        `${containerClass} span`
+      ) as HTMLElement;
+
+      expect(subtitle.textContent).toBe(
+        'O Duolingo for Schools utiliza o mesmo currículo do Duolingo e você ainda pode filtrar pelos padrões CEFR e ACTFL. Assim você encontra o conteúdo de que precisa para ensinar.'
+      );
+    });
+  });
+
+  describe('tests from section about fun and personalized tasks', () => {
+    const containerClass = '.personalized-tasks';
+
+    it('should have a title with contain text "tarefas divertidas e personalizadas"', () => {
+      const title = nativeElement.querySelector(
+        `${containerClass} h3`
+      ) as HTMLElement;
+
+      expect(title.textContent).toBe('tarefas divertidas e personalizadas');
+    });
+
+    it('should have a subtitle with contain text "As tarefas combinam o melhor da inteligência artificial..."', () => {
+      const subtitle = nativeElement.querySelector(
+        `${containerClass} span`
+      ) as HTMLElement;
+
+      expect(subtitle.textContent).toBe(
+        'As tarefas combinam o melhor da inteligência artificial e da ciência da aprendizagem para encontrar o nível e o ritmo certos para cada aluno. E eles adoram os desafios e os recursos que lembram jogos!'
+      );
+    });
+  });
+
+  describe('tests from section about informations and progress from students', () => {
+    const containerClass = '.informations-about-progress-students';
+
+    it('should have a title with contain text "informações sobre o progresso dos alunos"', () => {
+      const title = nativeElement.querySelector(
+        `${containerClass} h3`
+      ) as HTMLElement;
+
+      expect(title.textContent).toBe(
+        'informações sobre o progresso dos alunos'
+      );
+    });
+
+    it('should have a subtitle with contain text "Veja o desempenho dos seus alunos com informações como nível de acertos e tempo dedicado aos estudos."', () => {
+      const subtitle = nativeElement.querySelector(
+        `${containerClass} span`
+      ) as HTMLElement;
+
+      expect(subtitle.textContent).toBe(
+        'Veja o desempenho dos seus alunos com informações como nível de acertos e tempo dedicado aos estudos.'
+      );
+    });
   });
 });
